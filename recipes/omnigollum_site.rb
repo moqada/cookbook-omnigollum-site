f86cddef2e8b47ca59df10e826a6543f5104b6c7
moqada <moqada@gmail.com>
2013-01-23 06:27:36 +0900

    Add initialize wiki repository

--- recipes/omnigollum_site.rb
+++ recipes/omnigollum_site.rb
@@ -27,3 +27,24 @@ template '/home/gollum/omnigollum-site/config.ru' do
     group 'gollum'
     mode 0644
 end
+
+if node[:omnigollum_site][:wiki_source] == ""
+    bash 'initialize-wiki-repository' do
+        user 'gollum'
+        group 'gollum'
+        cwd '/home/gollum'
+        code <<-EOH
+            test -d $BASE_PATH || git init $BASE_PATH
+        EOH
+        environment 'BASE_PATH' => node[:omnigollum_site][:base_path]
+    end
+else
+    git 'sync-wiki-repository' do
+        repository node[:omnigollum_site][:wiki_source]
+        reference 'master'
+        action :sync
+        user 'gollum'
+        group 'gollum'
+        destination node[:omnigollum_site][:base_path]
+    end
+end
